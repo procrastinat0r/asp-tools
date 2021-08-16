bdd9a81fa0b0a13ab60676e8984e27fee256b780
procrastinat0r <37618509+procrastinat0r@users.noreply.github.com>
2021-08-16 11:11:29 +0200

    Setup of project

---
+++ README.md
@@ -0,0 +1,10 @@
+# asp-tools
+
+A set of Clojure code to support Answer Set Programming (ASP).
+
+For ian introduction refer to https://en.wikipedia.org/wiki/Answer_set_programming
+
+## Usage
+
+TODO
+
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject asp-tools "0.1.0-SNAPSHOT"
+  :description "A set of ASP tools"
+  :url "http://example.com/FIXME"
+  :license {:name "GPL-3"
+            :url "FIXME"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns asp-tools.core})
