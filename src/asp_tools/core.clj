3924e7c997496c372ec7202ea6a91e3658b1a0f8
procrastinat0r <37618509+procrastinat0r@users.noreply.github.com>
2021-08-17 12:15:00 +0200

    Convert 2 Atomic Constraints

--- src/asp_tools/core.clj
+++ src/asp_tools/core.clj
@@ -1,17 +1,16 @@
 (ns asp-tools.core
-  (:require [clojure.string :as str]))
+  (:require
+            [clojure.string :as str]))
 
 (defn dzn-to-lp-atomic_constraint
   "Convert an Atomic Constraint from DZN to LP format"
   [dzn-str]
-  (str/join
-   (concat
-    [(println-str "% atomic constraints")]
-    (for [[_ l r] (let [
-                        [_ constraints] (re-matches #"(?s)AtomicConstraints\s+=\s+\[\|(\s+\d+,\s+\d+\|)*\]\s*;.*" dzn-str)
-                        acs (when constraints (re-seq #"(\d+),\s+(\d+)\|" constraints))
-                        ]
-                    acs)
-
-          ]
-      (format "atomiccon(%s,%s).\n" l r)))))
+  (let [
+        [_ c](re-matches #"(?s)AtomicConstraints\s+=\s+\[\|(.*)\]\s*;.*" dzn-str)
+        acs (when c (re-seq #"(\d+),\s+(\d+)\|" c))
+        ]
+    (str/join
+     (concat
+      [(println-str "% atomic constraints")]
+      (for [[_ l r] acs]
+        (format "atomiccon(%s,%s).\n" l r))))))
--- test/asp_tools/core_test.clj
+++ test/asp_tools/core_test.clj
@@ -4,7 +4,8 @@
 
 (deftest dzn-to-lp-empty-atomic-constraint
   (testing "Convert empty Atomic Constraint from DZN to LP format"
-    (is (= (format "%% atomic constraints\n") (dzn-to-lp-atomic_constraint "AtomicConstraints =  [ ] ;")))))
+    (is (= (format "%% atomic constraints\n")
+           (dzn-to-lp-atomic_constraint "AtomicConstraints =  [ ] ;")))))
 
 (deftest dzn-to-lp-atomic-constraint
   (testing "Convert Atomic Constraint from DZN to LP format"
@@ -14,3 +15,11 @@
        (is (= (format "%% atomic constraints\natomiccon(14,16).\n")
               (dzn-to-lp-atomic_constraint dzn-str))))))
 
+(deftest dzn-to-lp-2-atomic-constraints
+  (testing "Convert multiple Atomic Constraints from DZN to LP format"
+     (let [dzn-str "AtomicConstraints =  [|
+                                           4, 9|
+                                           14, 16|];
+                                         "]
+       (is (= (format "%% atomic constraints\natomiccon(4,9).\natomiccon(14,16).\n")
+              (dzn-to-lp-atomic_constraint dzn-str))))))
