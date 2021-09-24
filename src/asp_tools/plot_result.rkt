e422f531c70d078642fc6ffa7e4dc5a10c5a0473
procrastinat0r <37618509+procrastinat0r@users.noreply.github.com>
2021-09-24 21:48:03 +0200

    Add plot utility

---
+++ src/asp_tools/plot_result.rkt
@@ -0,0 +1,34 @@
+#lang racket
+(require plot)
+
+(define input-file
+  (if (empty? (vector->list (current-command-line-arguments)))
+      "../../examples/plot_result/clingo_DL_V2_out2.txt"
+      (first (current-command-line-arguments))))
+  
+; get all the lines with optimization values from a file
+(define (get-opt-values file)
+  (filter (lambda (line) (regexp-match #rx"Optimization:" line))
+          (sequence->list (in-lines file))))
+  
+(define relevant-lines (call-with-input-file input-file get-opt-values))
+
+(define plot-values
+  (for/list ([line relevant-lines])
+    (let* ([match (regexp-match #px"(\\d*):(\\d{2})\\.(\\d{3}) Optimization: ([\\d ]+)" line)]
+           [time (+ (* (string->number (second match)) 60 1000)
+                    (* (string->number (third match)) 1000)
+                    (string->number (fourth match)))]
+           [values (map string->number (string-split (fifth match)))]
+           [opt-value (apply + values)])
+      (list time opt-value))))
+
+;plot-values
+
+(define opt-values (map second plot-values))
+
+;(plot (discrete-histogram plot-values))
+(plot (lines plot-values
+             #:y-min (* (apply min opt-values) 0.9)
+             #:y-max (* (apply max opt-values) 1.1)))
+
